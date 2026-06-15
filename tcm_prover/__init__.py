7e0f26e341fac7fc1fcc666460cb917f8beac99a
Fyre Mael <fyremaelstrom@gmail.com>
2026-06-15 14:18:11 -0700

    Add TCM prover fixture namespace

---
+++ tcm_prover/__init__.py
@@ -0,0 +1,5 @@
+"""Tropical Contraction Machine fixtures for MATHSOLVE.
+
+This namespace contains executable research fixtures. It does not define a
+trusted proof boundary; certified status is owned by MATHCERT.
+"""
