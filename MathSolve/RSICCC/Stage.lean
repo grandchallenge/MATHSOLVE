eacc7985723891db38d959d595acc178528d7be0
Fyre Mael <fyremaelstrom@gmail.com>
2026-09-15 22:16:10 -0700

    rsi-ccc: repair general guarded coherence proof

--- MathSolve/RSICCC/Stage.lean
+++ MathSolve/RSICCC/Stage.lean
@@ -202,7 +202,7 @@ theorem gfixAt_coherent {A : StageObj.{u}} (f : Hom (later A) A)
   | refl => exact A.restrict_refl _ _
   | @step n h ih =>
       calc
-        A.restrict (Nat.step h) (gfixAt f (n + 1))
+        A.restrict (h.trans (Nat.le_succ n)) (gfixAt f (n + 1))
             = A.restrict h
                 (A.restrict (Nat.le_succ n) (gfixAt f (n + 1))) := by
                   simpa using
