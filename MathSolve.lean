f4985f80aee5b2445fe746088d926823b9cb0c32
Fyre Mael <fyremaelstrom@gmail.com>
2026-09-18 05:29:02 -0700

    PNP: import Programme run-step normalization

--- MathSolve.lean
+++ MathSolve.lean
@@ -18,3 +18,4 @@ import MathSolve.PNP.TM2ProgrammeInvariant
 import MathSolve.PNP.ProgrammeStep
 import MathSolve.PNP.TM2CfgInvariant
 import MathSolve.PNP.TM2ProgrammeLocal
+import MathSolve.PNP.TM2ProgrammeRunStep
