e5a82e54b5465a173e959c889dada6250feb61ad
Fyre Mael <fyremaelstrom@gmail.com>
2026-09-19 06:01:24 -0700

    PNP: register reverse simulator machine at package root

--- MathSolve.lean
+++ MathSolve.lean
@@ -30,3 +30,4 @@ import MathSolve.PNP.TM2ProgrammeStartupRun
 import MathSolve.PNP.TM2ProgrammeRewindRun
 import MathSolve.PNP.TM2ForwardCompiler
 import MathSolve.PNP.ProgrammeTM2Types
+import MathSolve.PNP.ProgrammeTM2Machine
