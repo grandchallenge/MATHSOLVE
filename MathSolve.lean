32803bb7ee440333d0c7576163e5c2c508f3fa1c
Fyre Mael <fyremaelstrom@gmail.com>
2026-09-19 05:58:51 -0700

    PNP: register reverse simulator proof modules

--- MathSolve.lean
+++ MathSolve.lean
@@ -30,3 +30,5 @@ import MathSolve.PNP.TM2ProgrammeStartupRun
 import MathSolve.PNP.TM2ProgrammeRewindRun
 import MathSolve.PNP.TM2ForwardCompiler
 import MathSolve.PNP.ProgrammeTM2Types
+import MathSolve.PNP.ProgrammeTM2Machine
+import MathSolve.PNP.ProgrammeTM2Representation
